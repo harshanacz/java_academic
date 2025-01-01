35159114689dca7b9f8d01fc7207b217f692419d
Kavindu Harshana <45476434+harshanacz@users.noreply.github.com>
2025-01-01 15:08:09 +0530

    Q02 -PI

---
+++ Q02 - PI/Interfacer.java
@@ -0,0 +1,9 @@
+
+public class Interfacer {
+    public static void main(String[] args) {
+        Solver solver = new Solver();
+        int value = solver.method(3,4);
+        System.out.println("Value: " + value);
+    }
+}
+
---
+++ Q02 - PI/Solver.java
@@ -0,0 +1,8 @@
+public class Solver {
+
+    int method(int a, int b) {
+        int value = (a * a) + (b * b);
+        int c = (int) Math.sqrt(value); 
+        return c;
+    }
+}
