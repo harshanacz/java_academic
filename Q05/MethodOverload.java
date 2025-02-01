f53926c872fcaad85d6e2a3dd516f7e4a030b3da
Kavindu Harshana <45476434+harshanacz@users.noreply.github.com>
2025-02-01 10:34:40 +0530

    method overload

---
+++ Q05/MethodOverload.java
@@ -0,0 +1,13 @@
+public class MethodOverload {
+   
+    
+    static void print (String printValue){
+        System.out.println(printValue);
+    }
+    
+    static void print (int printValue){
+        System.out.println(printValue);
+    }
+    
+    
+}
